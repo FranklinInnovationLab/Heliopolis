47872ba0ae9606d5c33e9b0958c1f1af95e09703
DrakeW <junyuw@berkeley.edu>
2015-05-25 21:46:52 -0700

    minor fix

--- Helio/test/models/subscription_test.rb
+++ Helio/test/models/subscription_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: subscriptions
+#
+#  id             :integer          not null, primary key
+#  user_id        :integer
+#  business_id    :integer
+#  check_in_count :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 require 'test_helper'
 
 class SubscriptionTest < ActiveSupport::TestCase
